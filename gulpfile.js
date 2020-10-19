9a385221661370820f381a1c78cd84d3738de47d
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-10-19 23:26:55 +0300

    chore(gulp): setting gulp and add build to .gitirnore

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,12 @@ var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
+var posthtml = require("gulp-posthtml");
+var htmlmin = require("gulp-htmlmin");
+var include = require("posthtml-include");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("src/styles/index.scss")
@@ -17,13 +21,13 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "src/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -34,4 +38,38 @@ gulp.task("server", function () {
   gulp.watch("src/*.html").on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("html", function () {
+  return gulp.src("src/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "src/fonts/**/*.{woff,woff2,eot,svg}",
+    "src/assets/**",
+    "src/js/**",
+    "src/*.ico"
+  ], {
+    base: "src"
+  })
+  .pipe(gulp.dest("build"))
+})
+
+gulp.task("clean", function () {
+  return del("build");
+})
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "html"
+));
+
+gulp.task("start", gulp.series("build", "server"));
